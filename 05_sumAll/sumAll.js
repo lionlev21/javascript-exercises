ae56ae2a8e7b678a5053a28e25e94e710c086262
lionlev21 <66791558+lionlev21@users.noreply.github.com>
2023-10-08 16:24:58 +0300

    edit all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(!Number.isInteger(start) || !Number.isInteger(end))
+    {
+        return "ERROR";
+    }
+    if(start <0 || end<0)
+    {
+        return "ERROR";
+    }
+    if(start>end)
+    {
+        const temp = start;
+        start = end;
+        end = temp;
+    }
+    let sum = 0;
+while(start<=end)
+{
+    sum+=start;
+    start++;
+}
+return sum
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+   temperature =  (temperature-32) * (5/9);
+  temperature = Math.round (temperature*10) /10;
+  return temperature;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+temperature = temperature * (9/5) +32;
+temperature = Math.round (temperature*10) /10;
+  return temperature;
 };
 
 // Do not edit below this line
